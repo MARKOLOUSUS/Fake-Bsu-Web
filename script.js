e70a029d8c5a1d87435560742fdaad71e7d03277
MARKOLOUSUS <markdanielcomendador31@gmail.com>
2026-08-09 11:47:59 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,92 @@
+function showMVCV() {
+  const content = document.getElementById("content");
+
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+}
+
+const texts = [
+    "🎥Orientation for New Red Spartan Students🆕",
+    "📅Collegiate Calendar  2026-2027🆕",
+    "🚑Multi-Hazard Emergency Preparedness Guide🆕",
+    "☎️Emergency Contact Numbers🆕"
+];
+let index = 0;
+function updateText() {
+    document.getElementById("displayText").innerText = texts[index];
+}
+function nextText() {
+    index = (index + 1) % texts.length; // loop forward
+    updateText();
+}
+function prevText() {
+    index = (index - 1 + texts.length) % texts.length; // loop backward
+    updateText();
+}
+
+
+const images = [
+    "1.png",
+    "2.png",
+    "3.png",
+    "4.png",
+    "5.png"
+];
+
+let indexImg = 0;
+function updateImage() {
+    document.getElementById("slider").src = images[indexImg];
+}
+
+function nextImage() {
+    indexImg = (indexImg + 1) % images.length;
+    updateImage();
+}
+
+function prevImage() {
+    indexImg = (indexImg - 1 + images.length) % images.length;
+    updateImage();
+}
+
+function showHome() {
+  const content = document.getElementById("home");
+
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+}
+
+function showNews() {
+  const content = document.getElementById("news");
+
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+}
+
+function showAdvisories() {
+  const content = document.getElementById("advisories");
+
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+}
+
+function showSchedules() {
+  const content = document.getElementById("schedule");
+
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+}
