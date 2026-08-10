f477e0bd24619beccab6c0ea1a12a2be29f63559
MARKOLOUSUS <markdanielcomendador31@gmail.com>
2026-08-10 10:00:04 +0800

    Describe your changes

--- script.js
+++ script.js
@@ -29,11 +29,11 @@ function prevText() {
 
 
 const images = [
-    "1.png",
-    "2.png",
-    "3.png",
-    "4.png",
-    "5.png"
+    "images/1.png",
+    "images/2.png",
+    "images/3.png",
+    "images/4.png",
+    "images/5.png"
 ];
 
 let indexImg = 0;
